bc0991ccc1bd59d93ccd7e98e986afac8f09a188
siteusuarionovo <danillo.vilena@lwmail.com.br>
2022-11-10 15:24:53 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from tkinter.tix import Tree
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/servicos")
+def servicos():
+    return render_template("servicos.html")
+
+
+    
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
